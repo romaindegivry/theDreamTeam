773cc9a4aacc99825610b7d648c2d00a27973b3a
Clement De Givry <rrc14@ic.ac.uk>
2017-10-20 11:19:09 +0100

    created rosTools for simplification

---
+++ baselineScripts/rosTools.py
@@ -0,0 +1,87 @@
+import rospy
+import math
+import sys
+import time
+from mavros_msgs.msg import OpticalFlowRad #import optical flow message structure
+from mavros_msgs.msg import State  #import state message structure
+from sensor_msgs.msg import Range  #import range message structure
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose #import position message structures
+from geometry_msgs.msg import TwistStamped #used to set velocity messages
+from mavros_msgs.srv import *   #import for arm and flight mode setting
+
+
+
+class velControl:
+    def __init__(self, attPub):  #attPub = attitude publisher
+        self._attPub = attPub
+        self._setVelMsg = TwistStamped()
+        self._targetVelX = 0
+        self._targetVelY = 0
+        self._targetVelZ = 0
+
+    
+    def setVel(self, coordinates):
+        self._targetVelX = float(coordinates[0])
+        self._targetVelY = float(coordinates[1])
+        self._targetVelZ = float(coordinates[2])
+        rospy.logwarn("Target velocity is \nx: {} \ny: {} \nz: {}".format(self._targetVelX,self._targetVelY, self._targetVelZ))
+
+
+    def publishTargetPose(self, stateManagerInstance):
+        self._setVelMsg.header.stamp = rospy.Time.now()    #construct message to publish with time, loop count and id
+        self._setVelMsg.header.seq = stateManagerInstance.getLoopCount()
+        self._setVelMsg.header.frame_id = 'fcu'
+
+        self._setVelMsg.twist.linear.x = self._targetVelX
+        self._setVelMsg.twist.linear.y = self._targetVelY
+        self._setVelMsg.twist.linear.z = self._targetVelZ
+        
+        self._attPub.publish(self._setVelMsg)
+
+class stateManager: #class for monitoring and changing state of the controller
+    def __init__(self, rate):
+        self._rate = rate
+        self._loopCount = 0
+        self._isConnected = 0
+        self._isArmed = 0
+        self._mode = None
+    
+    def incrementLoop(self):
+        self._loopCount = self._loopCount + 1
+
+    def getLoopCount(self):
+        return self._loopCount
+
+    def stateUpdate(self, msg):
+        self._isConnected = msg.connected
+        self._isArmed = msg.armed
+        self._mode = msg.mode
+        rospy.logwarn("Connected is {}, armed is {}, mode is {} ".format(self._isConnected, self._isArmed, self._mode)) #some status info
+
+    def armRequest(self):
+        rospy.wait_for_service('/mavros/set_mode')
+        try:
+            modeService = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode) #get mode service and set to offboard control
+            modeService(custom_mode='OFFBOARD')
+        except rospy.ServiceException as e:
+            print("Service mode set faild with exception: %s"%e)
+    
+    def offboardRequest(self):
+        rospy.wait_for_service('/mavros/cmd/arming')
+        try:
+            arm = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool) #get arm command service and arm
+            arm(True)
+        except rospy.ServiceException as e:   #except if failed
+            print("Service arm failed with exception :%s"%e)
+
+
+    def waitForPilotConnection(self):   #wait for connection to flight controller
+        rospy.logwarn("Waiting for pilot connection")
+        while not rospy.is_shutdown():  #while not shutting down
+            if self._isConnected:   #if state isConnected is true
+                rospy.logwarn("Pilot is connected")
+                return True
+            self._rate.sleep
+        rospy.logwarn("ROS shutdown")
+        return False
