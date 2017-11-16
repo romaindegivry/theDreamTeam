8ca3a5fe313676c826b70722b026a463477af0e5
zhao96 <weezhao@live.com>
2017-11-16 18:20:22 +0000

    added bash folder

---
+++ bash_scripts/drone_launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Launch Gazebo"
+cd ~/src/Firmware
+gnome-terminal -e './gazeboVmWork.sh "make posix_sitl_default gazebo_iris_opt_flow"'
+sleep 10s
+echo "Launch Drone Node"
+cd ~/src/Firmware
+gnome-terminal -e './rosLaunchDrone.sh'
+sleep 10s
+echo "Launch Navigator"
+cd ~/catkin_ws/src/l3drone/scripts/theDreamTeam/baselineScripts
+#gnome-terminal -e 'python navigator.py | tee navigator_log.txt & disown'
---
+++ bash_scripts/run_core.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Launch Drone Node"
+cd ~/src/Firmware
+gnome-terminal -e './rosLaunchDrone.sh'
